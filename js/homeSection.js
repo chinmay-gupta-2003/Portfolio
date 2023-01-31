37dcfc96151ae32be00ba695b80089a19686181a
chinmay-gupta-2003 <guptachinmay2003@gmail.com>
2023-01-31 15:45:04 +0530

    refactored

--- js/homeSection.js
+++
@@ -1,5 +0,0 @@
-const homeText = document.querySelector('.home-text');
-
-const homeTextElements = homeText.querySelectorAll('p,h1,h2');
-
-homeTextElements.forEach((element) => (element.style.display = 'inline-block'));
